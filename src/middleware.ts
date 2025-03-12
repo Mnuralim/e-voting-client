1ef59e53bea1b4af58ea1ed9d01bf4f31a4b71d9
Mnuralim <shadownur345@gmail.com>
2025-03-12 22:57:25 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,19 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 import { decodeJwt } from "jose";
 
 export async function middleware(req: Request) {
   const { pathname } = new URL(req.url);
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("jwt")?.value;
+  // const cookieStore = cookies();
+  // const token = (await cookieStore).get("jwt")?.value;
+
+  const cookieHeader = req.headers.get("cookie");
+  console.log("Cookies from header:", cookieHeader);
+
+  const token = cookieHeader
+    ?.split("; ")
+    .find((c) => c.startsWith("jwt="))
+    ?.split("=")[1];
 
   console.log("Token:", token);
 
