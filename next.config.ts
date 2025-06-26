771f1455f40baf5f9bba5774d50f55ccd9de1caf
Mnuralim <shadownur345@gmail.com>
2025-06-26 09:39:56 +0700

    add new hostname to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
       {
         hostname: "715e18ea7a4b45c99b992e34fe0676c0.ipfscdn.io",
       },
+      {
+        hostname: "dee5d9eee9ca3346186cc8316a92eb9e.ipfscdn.io",
+      },
     ],
   },
 };
